0cac5503466a42d802801e1b804abe7930c26cef
Kipkiruie <elishak551@gmail.com>
2024-06-27 09:51:24 +0300

    add

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,36 +1,39 @@
 module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
-    },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: ['jest'],
-    rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
-    },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
-  };
-  
+	  env: {
+		      browser: false,
+		      es6: true,
+		      jest: true
+		    },
+	  extends: [
+		      'airbnb-base',
+		      'plugin:jest/all'
+		    ],
+	  globals: {
+		      Atomics: 'readonly',
+		      SharedArrayBuffer: 'readonly'
+		    },
+	  parserOptions: {
+		      ecmaVersion: 2018,
+		      sourceType: 'module'
+		    },
+	  plugins: ['jest'],
+	  rules: {
+		      'no-console': 'off',
+		      'no-shadow': 'off',
+		      'no-restricted-syntax': [
+			            'error',
+			            'LabeledStatement',
+			            'WithStatement'
+			          ],
+		      'no-multiple-empty-lines': ['error', { 'max': 0 }],
+		      'no-tabs': ['error'],
+		      'no-mixed-spaces-and-tabs': ['error']
+		    },
+	  overrides: [
+		      {
+			            files: ['*.js'],
+			            excludedFiles: 'babel.config.js'
+			          }
+		    ]
+};
+
